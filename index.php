ce3d54cd10972711a8affad057c0d6e3ac0139ad
Maciejziobroniewicz <m.ziobroniewicz@zset.leszno.pl>
2024-12-11 14:52:55 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -5,26 +5,46 @@
         <link rel="stylesheet" href="style.css">
         <link rel="icon" href="favicon.png">
         <title>Lepszy Jutub</title>
+
     </head>
     
     <body>
     <?php
     session_start();
+
+    $username = "root";
+    $password = "";
+    $servername = "localhost";
+    $db = "bilibili";
+
+    $conn = new mysqli($servername, $username, $password, $db);
+
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['videoId'])) {
+        $videoId = $_POST['videoId'];
+        if (filter_var($videoId, FILTER_VALIDATE_INT)) {
+            $stmt = $conn->prepare("UPDATE filmy SET wyswietlenia = wyswietlenia + 1 WHERE id_filmiku = ?");
+            $stmt->bind_param('i', $videoId);
+            $stmt->execute();
+            $stmt->close();
+            header("Location: video.php?id=$videoId");
+            exit;
+        }
+    }
     ?>
 
     <div class="header">
         <?php if (isset($_SESSION['username'])): ?>
             <div class="user-info">
-                <h1><a href="upload.php"><img src="upload_icon.png"/></a><span>Witaj, <?php echo htmlspecialchars($_SESSION['username']); ?>
+                <h1><a href="upload.php"><img src="upload_icon.png"/></a></h1>
+                <span>Witaj, <?php echo htmlspecialchars($_SESSION['username']); ?></span>
                 
-                </span>
                 <form action="logout.php" method="post" style="display:inline;">
                     <button type="submit" class="logout-btn">Wyloguj się</button>
                 </form>
-                </h1>
-                
-                
-                
             </div>
         <?php else: ?>
             <h1><a href="login.php"><img src="user.png" alt="Login"></a></h1><br>
@@ -32,21 +52,8 @@
        
     </div>
     
-    </div><br>
+    <div><br>
     <?php
-    $username = "root";
-    $password = "";
-    $servername = "localhost";
-    $db = "bilibili";
-
-    // Połączenie z bazą danych
-    $conn = new mysqli($servername, $username, $password, $db);
-
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-    }
-
-    // Zapytanie o losowy film
     $sql_random = "SELECT id_filmiku, plik, nazwa FROM filmy ORDER BY RAND() LIMIT 1";
     $result_random = $conn->query($sql_random);
 
@@ -57,14 +64,17 @@
         $video_random = $row_random['plik'];
         $title_random = $row_random['nazwa'];
         echo "<div style='margin-top:110px;'></div>";
-        echo "<a href='video.php?id=" . $id_filmiku . "'>";
+        echo "<form method='POST' action='' class='video-form'>";
+        echo "<input type='hidden' name='videoId' value='$id_filmiku'>";
+        echo "<button type='submit' class='video-link'>";
         echo "<div class='main_video'>";
         
-        echo "<video><source src='$video_random' type='video/mp4'></video>";
+        echo "<video controls><source src='$video_random' type='video/mp4'></video>";
         
         echo "<div class='title'>$title_random</div>";
         echo "</div>";
-        echo "</a>";
+        echo "</button>";
+        echo "</form>";
     } else {
         echo "Nie znaleziono żadnego filmu.";
     }
@@ -75,6 +85,7 @@
                 FROM filmy
                 ORDER BY wyswietlenia DESC";  
     $result_all = $conn->query($sql_all);
+    
 
     if ($result_all && $result_all->num_rows > 0) {
         echo "<div class='video-grid-container'>";
@@ -86,13 +97,16 @@
             $title_all = $row_all['nazwa'];
             $wyswietlenia_all = $row_all['wyswietlenia'];
 
-            echo "<a href='video.php?id=" . $id_filmiku . "'>";
+            echo "<form method='POST' action='' class='video-form'>";
+            echo "<input type='hidden' name='videoId' value='$id_filmiku'>";
+            echo "<button type='submit' class='video-link'>";
             echo "<div class='video-item'>";
-            echo "<video><source src='$video_all' type='video/mp4'></video>";
+            echo "<video controls><source src='$video_all' type='video/mp4'></video>";
             echo "<div class='title'>$title_all</div>";
             echo "<div class='views'>Wyświetlenia: $wyswietlenia_all</div>"; 
             echo "</div>";
-            echo "</a>";
+            echo "</button>";
+            echo "</form>";
         }
         
         echo "</div></div>";
@@ -100,6 +114,5 @@
         echo "Nie znaleziono żadnych filmików";
     }
     ?>
-
     </body> 
 </html>
